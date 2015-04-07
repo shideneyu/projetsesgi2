0f98fd8b542278b013654addceca4ccebca3c086
sidney <shideneyu@gmail.com>
2015-04-07 14:33:54 +0200

    Added hello

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta charset="UTF-8">
+    <title>Titre de ma page</title>
+    <meta name="description" content="contenu de ma page">
+  <body>
+	HELLO !!
+  </body>
+  </head>
+</html>
